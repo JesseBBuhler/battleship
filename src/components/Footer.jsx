64ccb8d2334845c03068e810a7ce5aafbab574fc
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-01-18 18:11:03 -0700

    create routing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Thanks for playing!</p>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/j-brayden-buhler/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://github.com/JesseBBuhler">Github</a>
+        </li>
+        <li>
+          <a href="https://braydenbuhler.com">Website</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
