985a2ae37e433ebd82d14fc17a8a400c6438e7d3
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-01-22 11:17:53 -0700

    ships can change direction

--- src/components/grid/ShipGrid.jsx
+++ src/components/grid/ShipGrid.jsx
@@ -1,14 +1,24 @@
 import "./ShipGrid.css";
+import { useState } from "react";
 
 const ShipGrid = () => {
+  const [battleship, setBattleship] = useState("left-right");
+
+  const changeDirection = () => {
+    battleship === "left-right"
+      ? setBattleship("top-bottom")
+      : setBattleship("left-right");
+  };
   return (
     <div className="grid oceanGrid">
-      <div className="gridSpacer">0</div>
-      <div className="ship one top-bottom"></div>
-      <div className="ship two top-bottom"></div>
-      <div className="ship three top-bottom"></div>
-      <div className="ship four left-right"></div>
-      <div className="ship five left-right"></div>
+      <div className="ship destroyer top-bottom"></div>
+      <div className="ship cruiser top-bottom"></div>
+      <div className="ship submarine top-bottom"></div>
+      <div
+        onClick={() => changeDirection()}
+        className={`ship battleship ${battleship}`}
+      ></div>
+      <div className="ship carrier left-right"></div>
     </div>
   );
 };
